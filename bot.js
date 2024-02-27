d1218bf72d86664cdb6bc227853e3c9e853d616a
LousyFox <xshadownightx12@gmail.com>
2024-02-28 00:56:29 +0700

    123

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
  
  
 app.get('/', function(request, response){ response.send(`Монитор активен. Локальный адрес: http://localhost:${port}`); });
